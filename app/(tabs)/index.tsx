695f586bca2b32d51cbe0b6bb6837d430e2b4667
Michael Youmans <myoumans2013@gmail.com>
2026-03-09 15:28:18 -0400

    Refactor home layout and add new bottom tabs (map, events, settings)

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,21 @@
 import EventBox from "@/components/home-event-box";
 import ScheduleBox from "@/components/home-schedule-box";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function Home() {
   return (
     <View style={styles.mainScreen}>
+      <Text
+        style={{
+          color: "white",
+          fontSize: 24,
+          marginBottom: 10,
+          position: "absolute",
+          top: 125,
+        }}
+      >
+        Hello #name
+      </Text>
       <ScheduleBox />
       <EventBox />
     </View>
