d135279dc439a32df3fd95d4c1c746c95cb4b354
Shambalam <dan.cararo@gmail.com>
2026-03-11 13:31:13 -0400

    started implementing the event page and event-box component which will house the event info seperate from home-event-box

--- app/(tabs)/events.tsx
+++ app/(tabs)/events.tsx
@@ -1,3 +1,22 @@
+import EventBox from "@/components/event-box";
+import { StyleSheet, View } from "react-native";
+
+
 export default function Events() {
-  return null;
+  return (
+      <View style={styles.eventScreen}>
+        <EventBox event={{}} />
+      </View>
+    );
 }
+const styles = StyleSheet.create({
+  eventScreen: {
+    flex: 1,
+    backgroundColor: "#20201B",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  textColor: {
+    color: "white",
+  },
+});
